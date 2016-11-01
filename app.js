0097e597566c92c3df7144af99f6b368f0b025d1
Long <long@Longs-Mac-mini.local>
2016-11-01 12:55:29 +0700

    add usd conversion

--- app.js
+++ app.js
@@ -9,21 +9,66 @@ var numeral = require('numeral');
 client.connect({ token: "MjMxMjc3MTA4NTE5NzYzOTc4.Cs-BKQ.Oy9JzukNhZ6Kc7aJJuj70hGQqYg" });
 
 client.Dispatcher.on(Events.GATEWAY_READY, e => {
-    console.log("Connected as: " + client.User.username);
+  console.log("Connected as: " + client.User.username);
 });
 
 client.Dispatcher.on(Events.MESSAGE_CREATE, e => {
-	var content = e.message.content.toLowerCase();
-    if (content.indexOf("mbtc = vnd") !== -1) {
-        var moni = parseInt(e.message.content.substring(0, content.indexOf("mbtc = vnd")));
-        request("https://api.coindesk.com/v1/bpi/currentprice/VND.json", function(error, response, body) {
+  var content = e.message.content.toLowerCase();
+  if (content.search(/!(\d*)mbtc/g) !== -1) {
+    var value = content.match(/!(\d*)mbtc/g);
+    for (var i = 0; i < value.length; i++) {
+      var moni = parseInt(value[i].substring(1, value[i].indexOf("mbtc")));
+      console.log(moni);
+      if (moni > 10000) {
+        e.message.channel.sendMessage(e.message.author.username + ", do you even have that much :moni: in VND? :hanz: :hanz:");
+      } else {
+        (function(tmp) {
+          request("https://api.coindesk.com/v1/bpi/currentprice/VND.json", function(error, response, body) {
             if (!error && response.statusCode == 200) {
-                var obj = JSON.parse(body);
-                var string = numeral(obj.bpi.VND.rate_float * moni / 1000).format('0,0');
-                if (string !== "NaN") {
-                    e.message.channel.sendMessage("Thưa anh " + e.message.author.username + ", bằng " + string + "đ ạ");
-                }
+              var obj = JSON.parse(body);
+              var string = numeral(obj.bpi.VND.rate_float * tmp / 1000).format('0,0');
+              var string2 = numeral(obj.bpi.USD.rate_float * tmp / 1000).format('0,0');
+              if (string !== "NaN") {
+                e.message.channel.sendMessage(e.message.author.username + ": " + tmp + " mbtc = " + string2 + " USD = " + string + "đ");
+              }
             }
-        });
-    }
+          });
+        })(moni);
+      }
+    };
+  }
+
+  if (content.search(/!(\d*)usd/g) !== -1) {
+    var value = content.match(/!(\d*)usd/g);
+    for (var i = 0; i < value.length; i++) {
+      var moni = parseInt(value[i].substring(1, value[i].indexOf("usd")));
+      console.log(moni);
+      if (moni > 10000) {
+        e.message.channel.sendMessage(e.message.author.username + ", do you even have that much :moni: in VND? :hanz: :hanz:");
+      } else {
+        (function(tmp) {
+          request("https://api.coindesk.com/v1/bpi/currentprice/VND.json", function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+              var obj = JSON.parse(body);
+              var string = numeral(obj.bpi.VND.rate_float * tmp / 1000).format('0,0');
+              var string2 = numeral((1 / obj.bpi.USD.rate_float) * tmp * 1000).format('0,0');
+              if (string !== "NaN") {
+                e.message.channel.sendMessage(e.message.author.username + ": " + tmp + " usd = " + string2 + " mbtc = " + string + "đ");
+              }
+            }
+          });
+        })(moni);
+      }
+    };
+  }
+
+  if (content.search(/!getbanpick/g) !== -1) {
+    var teamName = content.substring(content.search(/!getbanpick(.*)/g) + 11);
+    request("https://guarded-escarpment-56551.herokuapp.com/getABC/" + teamName, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var obj = JSON.parse(body);
+        e.message.channel.sendMessage(teamName.toUpperCase() + "\n" + "Banned \n" + JSON.stringify(obj.banned) + "\n" + "Picked \n" + JSON.stringify(obj.picked));
+      }
+    });
+  }
 });
